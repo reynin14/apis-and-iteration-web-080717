44a9e40ac30e0cb41e4ea1f5f8db3f21bc1aba58
Kevin <kyonzon14@gmail.com>
2017-08-08 14:24:55 -0400

    final

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,21 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+
+  return_value = nil
+
+  character_hash["results"].each do |character_info|
+    if character_info["name"] == character
+      return_value = character_info["films"]
+    end
+  end
+  x = return_value.collect do |film_info|
+    film = RestClient.get("#{film_info}")
+    JSON.parse(film)
+  end
+  x
+end
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,17 +30,20 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
-end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.collect do |movie|
+    movie["title"]
+  end.join(", ")
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  puts parse_character_movies(films_hash)
 end
 
+show_character_movies("Luke Skywalker")
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "YO!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
+  puts "what character you want"
+  response = gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
 end
